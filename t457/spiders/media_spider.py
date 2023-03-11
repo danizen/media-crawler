6f045d3ec9ee53133bcbacfd95a4ad31e18c05db
Dan Davis <dan@danizen.net>
2023-03-10 23:43:37 -0500

    Complete crawler.

--- t457/spiders/media_spider.py
+++ t457/spiders/media_spider.py
@@ -1,7 +1,31 @@
+import logging
+from functools import cached_property
+from urllib.parse import urljoin, urlsplit, urlunsplit
 from scrapy import Request
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from scrapy.spiders import SitemapSpider
-from t457.utils import normalize_url
+
 from t457.items import T457Item
+
+LOG = logging.getLogger(__name__)
+
+
+def clean_wixstatic_url(url):
+    p = urlsplit(url)
+    if p.netloc == 'static.wixstatic.com':
+        # process to remove resizing, version, and fillcomponents
+        last = p.path.split('/')[-1]
+        pos = p.path.find(last)
+        if pos > -1:
+            path = p.path[0:pos]+last
+            url = urlunsplit((
+                p.scheme,
+                p.netloc,
+                path,
+                p.query,
+                ''
+            ))
+    return url
 
 
 class MediaSpider(SitemapSpider):
@@ -11,27 +35,54 @@ class MediaSpider(SitemapSpider):
         'www.t457.org',
         'static.wixstatic.com',
     ]
+    download_path = None
     sitemap_urls = [
         'https://www.t457.org/sitemap.xml',
     ]
 
-    def start_requests(self):
-        return super().start_requests()
+    @cached_property
+    def media_method(self):
+        if self.download_path is not None:
+            return 'GET'
+        else:
+            return 'HEAD'
+    
+    @cached_property
+    def anchors(self):
+        return LxmlLinkExtractor(
+            allow_domains=self.allowed_domains,
+            canonicalize=True,
+            unique=True
+        )
+        
+    @cached_property
+    def media(self):
+        return LxmlLinkExtractor(
+            allow_domains=self.allowed_domains,
+            tags=('img',),
+            attrs=('src',), 
+            canonicalize=True,
+            unique=True,
+            deny_extensions=[]
+        )
 
     def parse(self, response):
-        base_url = response.css('head > base').attrib.get('href', response.url)
+        item = T457Item.load_from(response)
+        yield item
 
-        for selector in response.css('a'):
-            url = selector.attrib.get('href')
-            if url is not None and 'javascript:' not in url:
-                norm_url = normalize_url(base_url, url)
-                yield Request(norm_url, self.parse)
-                
-        for selector in response.css('img'):
-            url = selector.attrib.get('src')
-            if url is not None and 'javascript:' not in url:
-                norm_url = normalize_url(base_url, url)
-                yield Request(norm_url, self.parse_media, method='HEAD')
+        base_url = response.css('head > base').attrib.get('href', response.url)
+        for link in self.anchors.extract_links(response):
+            # not sure if scrapy does the urljoin
+            LOG.info('anchor link: %s', link.url)
+            url = urljoin(base_url, link.url)
+            yield Request(url, self.parse)
+
+        for link in self.media.extract_links(response):
+            LOG.info('media link: %s', link.url)
+            url = urljoin(base_url, link.url)
+            # link extractor will definitely clean-up the versioning
+            url = clean_wixstatic_url(url)
+            yield Request(url, self.parse_media, method=self.media_method)
         
     def parse_media(self, response):
         item = T457Item.load_from(response)
--- t457/utils.py
+++ t457/utils.py
@@ -1,35 +1,16 @@
-from urllib.parse import (
-    parse_qsl,
-    urljoin,
-    urlsplit, 
-    urlunsplit,
-)
+from urllib.parse import urljoin, urlsplit, urlunsplit
+from w3lib.url import canonicalize_url
 
 
-def normalize_url(site_url, url):
+def normalize_url(base_url, url):
+    url = urljoin(base_url, url)
+    return canonicalize_url(url)
+
+
+def normalize_media_url(base_url, url):
+    url = urljoin(base_url, url)
+    url = canonicalize_url(url)
     parts = urlsplit(url)
-
-    # lower case the scheme and the host
-    scheme = parts.scheme.lower()
-    netloc = parts.netloc.lower()
     
-    # TODO: uppercase escape sequences
-    
-    # TODO: remove default port
-    
-    # TODO: decode unreserved characters
-    
-    # TODO: encode non-URI characters
-    
-    # TODO: encode spaces
-    
-    # if there is a query string, sort the parameters and values to normalize the URL
-    query_string = parts.query
-    if query_string:
-        query_string = '&'.join(f'{nm}={val}' for nm, val in sorted(parse_qsl(query_string)))
-        
-    # recombine the URL ignoring any fragment
-    rel_uri = urlunsplit((scheme, netloc, parts.path, query_string, ''))
-
-    # and make absolute
-    return urljoin(site_url, rel_uri)
+    path = parts.path
+    return url
